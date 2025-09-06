100b5e360910464ecf9614e38639c09c788c7d3d
Ashan Ghimire <ashanghimire10@gmail.com>
2025-09-07 00:00:59 +0545

    Apparently, using password_hash three times increase the response time from 10ms avg to 200ms avg. So commented it out for now

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -12,37 +12,37 @@ class UserController extends AbstractController
     public function index(): Response
     {
 
-        $users = [
-            [
-                'first_name' => 'John',
-                'last_name' => 'Doe',
-                'email' => 'test@example.com',
-                'is_active' => 1,
-                'profile_picture' => 'john.jpg',
-                'password' => password_hash('securepassword', PASSWORD_BCRYPT),
-            ],
-            [
-                'first_name' => 'Jane',
-                'last_name' => 'Smith',
-                'email' => 'test2@example.com',
-                'is_active' => 0,
-                'profile_picture' => 'jane.jpg',
-                'password' => password_hash('anotherpassword', PASSWORD_BCRYPT),
-            ],
-            [
-                'first_name' => 'Bob',
-                'last_name' => 'Brown',
-                'email' => 'test3@example.com',
-                'is_active' => 1,
-                'profile_picture' => 'bob.jpg',
-                'password' => password_hash('yetanotherpassword', PASSWORD_BCRYPT),
-            ],
+        /* $users = [ */
+        /*     [ */
+        /*         'first_name' => 'John', */
+        /*         'last_name' => 'Doe', */
+        /*         'email' => 'test@example.com', */
+        /*         'is_active' => 1, */
+        /*         'profile_picture' => 'john.jpg', */
+        /*         'password' => password_hash('securepassword', PASSWORD_BCRYPT), */
+        /*     ], */
+        /*     [ */
+        /*         'first_name' => 'Jane', */
+        /*         'last_name' => 'Smith', */
+        /*         'email' => 'test2@example.com', */
+        /*         'is_active' => 0, */
+        /*         'profile_picture' => 'jane.jpg', */
+        /*         'password' => password_hash('anotherpassword', PASSWORD_BCRYPT), */
+        /*     ], */
+        /*     [ */
+        /*         'first_name' => 'Bob', */
+        /*         'last_name' => 'Brown', */
+        /*         'email' => 'test3@example.com', */
+        /*         'is_active' => 1, */
+        /*         'profile_picture' => 'bob.jpg', */
+        /*         'password' => password_hash('yetanotherpassword', PASSWORD_BCRYPT), */
+        /*     ], */
+        /**/
+        /* ]; */
 
-        ];
-
-        foreach ($users as $user) {
-            // User::create($user);
-        }
+        /* foreach ($users as $user) { */
+        /*     // User::create($user); */
+        /* } */
 
         $users = [
             [
