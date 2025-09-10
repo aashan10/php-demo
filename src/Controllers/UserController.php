a073a05d0e3453a0f9ddef13d01a75c97e320d0b
Ashan Ghimire <ashanghimire10@gmail.com>
2025-09-10 23:58:14 +0545

    Added User page

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Models\User;
+use Elementary\Http\Request;
 use Elementary\Http\Response;
+use Elementary\Utils\FlashBag;
+use Elementary\Utils\SessionBag;
+use Elementary\Validation\Validator;
 
 class UserController extends AbstractController
 {
@@ -65,5 +69,51 @@ class UserController extends AbstractController
 
         // This render method now comes from the updated AbstractController
         return $this->render('users/index', ['users' => $users]);
+    }
+
+    public function create(): Response
+    {
+        return $this->render('users/create');
+    }
+
+    public function store(Request $request, FlashBag $flashBag): Response 
+    {
+        $data = $request->post->only([
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'password_confirmation'
+        ]);
+
+        $validator = new Validator($data, [
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'minLength:8'],
+            'password_confirmation' => ['required', 'equals:password'],
+        ]);
+
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+
+            return $this->render('users/create', [
+                'errors' => $errors,
+                'old' => $data,
+            ]);
+        }
+
+        User::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+            'password' => password_hash($data['password'], PASSWORD_BCRYPT),
+            'is_active' => 1,
+            'profile_picture' => 'default.jpg',
+        ]);
+        $flashBag->add('success', 'User created successfully!');
+
+        return $this->redirectToRoute('users.index');
     }
 }
